b6fc97517bf3b0dcbd5a8ba88199af9c110710d1
huynh van hao <magicmath2k5@gmail.com>
2026-06-21 13:01:13 +0700

    done-706

---
+++ DesignHashMap/src/main/java/com/MyHashMap.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.Arrays;
+
+public class MyHashMap {
+    private int[] map = new int[1000001];
+
+    public MyHashMap() {
+        Arrays.fill(map, -1);
+
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+
+    }
+
+    public int get(int key) {
+        return map[key];
+
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
