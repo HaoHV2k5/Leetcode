86ffee753c6acea3dc23bf7629441fe08822b408
huynh van hao <magicmath2k5@gmail.com>
2026-07-29 13:00:09 +0700

    done - 153 (c2)

--- FindMinimumInRotatedSortedArray/src/main/java/com/Main.java
+++ FindMinimumInRotatedSortedArray/src/main/java/com/Main.java
@@ -25,6 +25,28 @@ public class Main {
         return ans;
     }
 
+    // cách 2
+    public int findMin2(int[] nums) {
+
+        int l = 0;
+        int r = nums.length - 1;
+
+
+        while(l < r){
+            int mid = l + (r - l )/2;
+
+            if(nums[mid] > nums[r] ){
+                l = mid +1;
+
+            }
+            else{
+                r = mid;
+
+            }
+        }
+        return nums[l];
+    }
+
 
     static void main() {
 
