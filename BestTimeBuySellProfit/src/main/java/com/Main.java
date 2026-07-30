eb2c9b9c9b81f85231fcbfdce3e8a6e610d9f25a
huynh van hao <magicmath2k5@gmail.com>
2026-07-30 22:24:37 +0700

    done - 121 c2

--- BestTimeBuySellProfit/src/main/java/com/Main.java
+++ BestTimeBuySellProfit/src/main/java/com/Main.java
@@ -15,6 +15,23 @@ public class Main {
         }
         return ans;
     }
+    // c2: sliding windown
+    public int maxProfit2(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int ans = 0;
+        int len = prices.length;
+        while(r < len){
+            if(prices[r] > prices[l]){
+                ans = Math.max(ans, prices[r] - prices[l]);
+            }
+            else{
+                l = r;
+            }
+            r++;
+        }
+        return ans;
+    }
     static void main() {
 
     }
